b7b703ca1c767ba7cab40702303af8d89dd1d3bf
Gitansh Arora <gitansh2003@gmail.com>
2023-03-29 22:06:50 +0530

    asd

---
+++ source.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import *
+master = Tk()
+# sets the geometry of main
+# root window
+master.geometry("450x300")
+master.configure(background="light green")
+btn = Button(master,text ="Krypton Calculator")
+btn.pack(padx=5,pady = 50)
+mainloop()
